d3e9a96e71ea37e301140de4dbd3275bd02aef18
zak39 <fotia.baptiste@hotmail.com>
2021-03-24 15:54:09 +0100

    The GeneralManagerMiddleware return an Exception if user isn't in GeneralManager group

---
+++ lib/Middleware/Exceptions/NotGeneralManagerException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Workspace\Middleware\Exceptions;
+
+use OCP\AppFramework\Http;
+
+class NotGeneralManagerException extends \Exception {
+    public function __construct()
+    {
+        parent::__construct('This user doesn\'t belong to General Manager group', Http::STATUS_FORBIDDEN);
+    }
+}
--- lib/Middleware/GeneralManagerMiddleware.php
+++ lib/Middleware/GeneralManagerMiddleware.php
@@ -4,22 +4,46 @@ namespace OCA\Workspace\Middleware;
 
 use OCP\AppFramework\Middleware;
 use OCP\IGroupManager;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\RedirectResponse;
+use OCP\IURLGenerator;
 use OCP\IUser;
+use OCA\Workspace\Middleware\Exceptions\NotGeneralManagerException;
 
 class GeneralManagerMiddleware extends Middleware{
 
-    private const GENERAL_MANAGER = "GeneralManager";
+    private $GENERAL_MANAGER = "GeneralManager";
 
-    private $group;
+    private $groupManager;
 
-    // public function __construct(IGroupManager $group)
-    // {
-    //     $this->group = $group;
-    // }
+    public function __construct(IGroupManager $groupManager, IURLGenerator $urlGenerator)
+    {
+        $this->groupManager = $groupManager;
+        $this->urlGenerator = $urlGenerator;
+    }
 
-    public function beforeController($controller, $methodName, IUser $userId ){
+    public function beforeController($controller, $methodName, string $userId ){
 
-        var_dump("coucou");
+        if(! $this->groupManager->isInGroup($userId, $this->GENERAL_MANAGER)){
+
+            throw new NotGeneralManagerException();
+
+        }        
 
     }
+
+    // TODO: Find a solution to use this method.
+    public function afterException($controller, $methodName, \Exception $exception): RedirectResponse{
+        if($exception instanceof NotGeneralManagerException){
+            var_dump('coucou');
+            $route = 'workspace.page.errorAccess';
+            $params = [];
+            
+            $url = $this->urlGenerator->linkToRoute($route, $params);
+
+            return new RedirectResponse($url);
+
+        }
+    }
+
 }
