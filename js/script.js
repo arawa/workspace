8e14791f7760dbcafd1110340dd99aa80eed5fe9
zak39 <fotia.baptiste@hotmail.com>
2021-03-26 18:21:03 +0100

    When we submit the form, the creating is made with javascript (using Promise and async/await). The groupfolder and group are created then the system adds the group in the groupfolder

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const btnGFElt = document.getElementById('btnGF');
+const btnGroupFolderElt = document.getElementById('btnGF');
 const btnGroupElt = document.getElementById('btnGroup');
+const inputGroupFoldrElt = document.getElementById('inputGF');
+const form = document.getElementById('workspaceform');
 
-btnGFElt.addEventListener('click', function(e){
+btnGroupFolderElt.addEventListener('click', function(e){
 
-    const inputGFElt = document.getElementById('inputGF');
+    const inputGroupFoldrElt = document.getElementById('inputGF');
 
     var request = new XMLHttpRequest();
     request.open('POST', 'https://nc21.dev.arawa.fr/apps/groupfolders/folders');
     request.setRequestHeader('OCS-APIRequest', 'true');
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    request.send( 'mountpoint=' + inputGFElt.value );
+    request.send( 'mountpoint=' + inputGroupFoldrElt.value );
 });
 
 btnGroupElt.addEventListener('click', function(e){
@@ -24,3 +26,73 @@ btnGroupElt.addEventListener('click', function(e){
     requestGroup.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     requestGroup.send( "groupid=" + inputGroupElt.value );
 });
+
+inputGroupFoldrElt.addEventListener('input', function(e){
+    var espaceManagerName = document.getElementById('espaceManagerName');
+    espaceManagerName.value = e.target.value;
+});
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+
+    let folderId = null;
+
+    if(form.mountpoint.value !== ""){
+        folderId = await createGroupFolder(form.mountpoint.value);
+    }
+    
+    if(form.espaceManagerName.value !== ''){
+        createGroupEspaceManager(form.espaceManagerName.value);
+    }
+
+    if(form.espaceManagerName.value !== '' && folderId !== null){
+        addEspaceManagerToGroupFolder(form.espaceManagerName.value, folderId);
+    }
+
+});
+
+const createGroupFolder = (mountpoint) => {
+
+    const myHeaders = new Headers();
+    myHeaders.append('OCS-APIRequest', 'true');
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    return fetch( 
+        'https://nc21.dev.arawa.fr/apps/groupfolders/folders',
+        { method: 'POST', headers: myHeaders, body: "mountpoint=" + mountpoint   }
+    )
+    .then(response => response.text())
+    .then(xmlString => $.parseXML(xmlString))
+    .then(function(data){
+            return data.documentElement.childNodes[3].childNodes[1].textContent
+        }
+    );
+}
+
+function createGroupEspaceManager(groupname){
+
+    var requestGroup = new XMLHttpRequest();
+    requestGroup.open('POST', 'https://nc21.dev.arawa.fr/ocs/v1.php/cloud/groups');
+    requestGroup.setRequestHeader('OCS-APIRequest', 'true');
+    requestGroup.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    requestGroup.send( "groupid=" + 'GE-' + groupname );
+}
+
+function addEspaceManagerToGroupFolder(gid, folderId){
+
+    const myHeaders = new Headers();
+    myHeaders.append('OCS-APIRequest', 'true');
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+
+
+    fetch(
+        'https://nc21.dev.arawa.fr/apps/groupfolders/folders/' + folderId + '/groups',
+        { method: 'POST', headers: myHeaders, body: "group="+ "GE-" + gid }
+        )
+        .then(
+            console.log('Add an Espace Manager to groupfolder : Succed !')
+        )
+        .catch(
+            console.log('Add an Espace Manager to groupfolder : Failled !')
+        );
+}
