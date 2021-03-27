7aa2d3ebd1075ba1708ee20843b32c733c7896b0
Cyrille Bollu <cyr.debian@bollu.be>
2021-03-27 23:53:19 +0100

    Insert MultiSelect component at the bottom of the users list
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/SpaceDetails.vue
+++ src/SpaceDetails.vue
@@ -27,7 +27,7 @@
 					<tr v-for="user in space.users"
 						:key="user.name">
 						<td> {{ user.name }} </td>
-						<td> {{ user.role }} </td>
+						<td> {{ t('workspace', user.role) }} </td>
 						<td> {{ user.email }} </td>
 						<td>
 							<Actions>
@@ -44,6 +44,17 @@
 							</Actions>
 						</td>
 					</tr>
+					<tr>
+						<td>
+							<Multiselect
+								id="newUser"
+								v-model="selectedUsers"
+								:options="selectableUsers"
+								:multiple="true"
+								:placeholder="t('workspace', 'Select new user')"
+								@search-change="lookupUsers" />
+						</td>
+					</tr>
 				</tbody>
 			</table>
 		</div>
@@ -53,11 +64,17 @@
 <script>
 import Actions from '@nextcloud/vue/dist/Components/Actions'
 import ActionButton from '@nextcloud/vue/dist/Components/ActionButton'
+import Multiselect from '@nextcloud/vue/dist/Components/Multiselect'
+// import debouncePromise from 'debounce-promise'
+import { generateOcsUrl } from '@nextcloud/router'
+import xmlToJSON from 'xmltojson'
+
 export default {
 	name: 'SpaceDetails',
 	components: {
 		Actions,
 		ActionButton,
+		Multiselect,
 	},
 	props: {
 		space: {
@@ -65,7 +82,41 @@ export default {
 			required: true,
 		},
 	},
+	data() {
+		return {
+			selectedUsers: [],
+			selectableUsers: [],
+		}
+	},
 	methods: {
+		lookupUsers(term) {
+			// safeguard for initialisation
+			if (term === undefined || term === '') {
+				return
+			}
+
+			// TODO: Users must be filtered to only those groups used in this EP
+			// TODO: limit max results?
+			// fetch users using OCS API
+			let url = generateOcsUrl('/core/autocomplete/get?search=' + term, 2)
+			url = url.endsWith('/') ? url.slice(0, -1) : url
+			fetch(url, {
+				headers: { 'OCS-APIRequest': 'true' },
+			})
+				.then((resp) => resp.text())
+				.then(text => {
+					const data = xmlToJSON.parseString(text).ocs[0].data[0].element
+					this.selectableUsers = data.map(user => {
+						// eslint-disable-next-line
+						console.log('user', user)
+						return {
+							label: user.label[0]._text
+						}
+					})
+					// eslint-disable-next-line
+					console.log(this.selectableUsers)
+				})
+		},
 		deleteUser() {
 			// TODO
 		},
