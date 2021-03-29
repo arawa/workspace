5accff8071967edaaa0c6cfbff49ee7391bfb7fc
Cyrille Bollu <cyr.debian@bollu.be>
2021-03-29 09:06:52 +0200

    Uses IUserManager to lookup users
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -4,35 +4,61 @@ namespace OCA\Workspace\Controller;
 use OCA\Workspace\AppInfo\Application;
 use OCP\IRequest;
 use OCP\IUserManager;
+use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Controller;
+use OCP\IUser;
+use OCP\IUserManager;
 use OCP\Util;
 
 class PageController extends Controller {
 	
 	private $userId;
 
-	protected $userManager;
+	/** @var IUserManager */
+	private $userManager;
 
-	public function __construct($AppName, IRequest $request, $UserId, IUserManager $users){
-		parent::__construct($AppName, $request);
-		$this->userId = $UserId;
-		$this->userManager = $users;
+	public function __construct(IRequest $request,
+			IUserManager $userManager){
+		parent::__construct(Application::APP_ID, $request);
+		$this->userManager = $userManager;
 	}
 
 	/**
-	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
-	 *          required and no CSRF check. If you don't know what CSRF is, read
-	 *          it up in the docs or you might create a security hole. This is
-	 *          basically the only required method to add this exemption, don't
-	 *          add it to any other method if you don't exactly know what it does
+	 * Application's main page
 	 *
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
 	public function index() {
-		Util::addScript(Application::APP_ID, 'workspace-main');		// js/main.js
+		Util::addScript(Application::APP_ID, 'workspace-main');		// js/workspace-main.js
 		return new TemplateResponse('workspace', 'index');  	// templates/index.php
 	}
+
+	/**
+	 * Returns a list of users whose name matches $term
+	 *
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 *
+	 * @param string $term
+	 * @return JSONResponse
+	 */
+	public function autoComplete(string $term) {
+		// lookup users
+		$users = $this->userManager->searchDisplayName($term);
+
+		// transform in a format suitable for the app
+		$data = [];
+		foreach($users as $user) {
+			array_push($data, [
+				'displayName' => $user->getDisplayName(),
+			]);
+		}
+
+		// return info
+		return new JSONResponse($data);
+	}
+
 
 }
