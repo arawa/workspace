7d769b746c12d2ca124959f2f326300720427f52
Cyrille Bollu <cyr.debian@bollu.be>
2021-03-29 23:21:13 +0200

    Some more styling of the SelectUsers modal
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/SelectUsers.vue
+++ src/SelectUsers.vue
@@ -7,7 +7,7 @@
   -->
 
 <template>
-	<div id="select-users">
+	<div class="select-users-wrapper">
 		<Multiselect
 			id="newUser"
 			v-model="selectedUsers"
@@ -16,11 +16,13 @@
 			:multiple="true"
 			:placeholder="t('workspace', 'Select new user')"
 			@search-change="lookupUsers" />
-		<div v-for="user in allSelectedUsers"
-			:key="user.name">
-			<span> {{ user.name }} </span>
+		<div class="select-users-list">
+			<div v-for="user in allSelectedUsers"
+				:key="user.name">
+				<span> {{ user.name }} </span>
+			</div>
 		</div>
-		<div>
+		<div class="select-users-actions">
 			<Actions>
 				<ActionButton
 					icon="icon-add"
@@ -74,3 +76,18 @@ export default {
 	},
 }
 </script>
+
+<style>
+.select-users-actions {
+	display: flex;
+	flex-flow: row-reverse;
+}
+.select-users-wrapper {
+	margin: 10px;
+}
+.select-users-list {
+	min-height: 400px;
+	max-height: 400px;
+	min-width: 500px;
+}
+</style>
