8a1f0a2194d189bd38136692286d35e8e9fbee09
zak39 <fotia.baptiste@hotmail.com>
2021-04-12 12:51:32 +0200

    The form to create sub group is availabled

---
+++ templates/content/subgroup.php
@@ -0,0 +1,22 @@
+<h1 class="inlineblock"><?php p($l->t('Create subgroup')); ?></h1>
+
+<div class="section">
+
+    <form action="#" id="createSubGroup">
+        <select name="selectCreateSubGroup" id="selectCreateSubGroup">
+            <option value="None">None</option>
+        </select>
+
+        <br>
+        
+        <input type="text" id="subGroup" name="subGroup" value="wsp_" disabled style="width:320px;">
+
+        <div>
+            <p></p>
+        </div>
+
+        <button type="sumbit">Create</button>
+
+    </form>
+
+</div>
