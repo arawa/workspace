292b60a99cca65e53f8ae7703d3239306a7cf933
zak39 <fotia.baptiste@hotmail.com>
2021-04-29 17:22:36 +0200

    List all users with theirs email address

--- src/App.vue
+++ src/App.vue
@@ -49,15 +49,15 @@
 									@tag="setSpaceQuota(name, $event)" />
 							</td>
 						</tr>
-						<tr v-for="(workspace, index) in workspaces" :key="index">
-							<td>{{ workspace.mount_point }}</td>
-							<td>{{ index }}</td>
+						<tr v-for="(workspace, index) in workspaces.result" :key="index">
+							<td> {{ workspace.mount_point }} </td>
+							<td> {{ adminUsers(workspace).join(', ') }} </td>
 							<td>
 								<Multiselect
 									class="quota-select"
 									tag-placeholder="t('workspace', 'Add specific quota')"
 									:taggable="true"
-									:value="workspace.quota"
+									:value="convertByteToGigaByte(workspace.quota)"
 									:options="['1GB', '5GB', '10GB', 'unlimited']"
 									@change="setSpaceQuota(index, $event)"
 									@tag="setSpaceQuota(index, $event)" />
@@ -98,21 +98,12 @@ export default {
 	data() {
 		return {
 			selectedSpaceName: 'all',
-			workspaces: undefined,
+			workspaces: { },
+			groupfolders: undefined,
 		}
 	},
-	beforeCreate() {
-		axios.get(OC.generateUrl('apps/groupfolders/folders'))
-			.then(response => {
-				this.workspaces = response.data.ocs.data
-				const spaces = response.data.ocs.data
-				console.debug(typeof (spaces))
-				for (const workspace of spaces) {
-					console.debug(workspace)
-				}
-			})
-	},
 	created() {
+		console.debug('created')
 		// TODO: spaces should be retrieved from groupfolders' API
 		Vue.set(this.$root.$data.spaces, 'spaceA', {
 			users: [
@@ -150,10 +141,36 @@ export default {
 			quota: '10GB',
 		})
 	},
+	async beforeMount() {
+		const resGroupfodlersApi = await axios.get(OC.generateUrl('apps/groupfolders/folders'))
+		const resGroupfoldersApiWithUsers = await this.searchUsersForGroupfolders(resGroupfodlersApi.data.ocs.data)
+		Vue.set(this.workspaces, 'result', resGroupfoldersApiWithUsers)
+	},
 	methods: {
+		convertByteToGigaByte(bytes) {
+			if (bytes > 0) {
+				const gb = (parseInt(bytes) / Math.pow(1024, 3))
+				return gb.toString() + 'GB'
+			} else {
+				return 'unlimited'
+			}
+		},
+		searchUsersForGroupfolders(groupfolders) {
+			for (const i in groupfolders) {
+				groupfolders[i].users = []
+				const groups = Object.keys(groupfolders[i].groups)
+				for (const group of groups) {
+					axios.get(OC.generateUrl(`apps/workspace/group/${group}/users`))
+						.then(responseUsers => {
+							groupfolders[i].users = groupfolders[i].users.concat(responseUsers.data)
+						})
+				}
+			}
+			return groupfolders
+		},
 		// Returns the list of administrators of a space
 		adminUsers(space) {
-			return space.users.filter((u) => u.role === 'admin').map((u) => u.name)
+			return space.users.filter((u) => u.role === 'admin').map((u) => u.email)
 		},
 		// Create a new space
 		onNewSpace(spaceName) {
