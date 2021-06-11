17366b4aa35b3e0db529dbbc265c7e19f591e97d
Cyrille Bollu <cyr.debian@bollu.be>
2021-06-11 12:26:30 +0200

    Adds unit tests for vuex store's getters
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getters } from './getters'
 import mutations from './mutations'
 import { generateUrl } from '@nextcloud/router'
 import axios from '@nextcloud/axios'
@@ -82,32 +83,5 @@ export default new Vuex.Store({
 			})
 		},
 	},
-	getters: {
-		// Returns the number of users in a group
-		groupUserCount: state => (spaceName, groupName) => {
-			const users = state.spaces[spaceName].users
-			if (users.length === 0) {
-				return 0
-			} else {
-				// We count all users in the space who have 'groupName' listed in their 'groups' property
-				return Object.values(users).filter(user => user.groups.includes(groupName)).length
-			}
-		},
-		// Returns the number of users in a space
-		spaceUserCount: state => name => {
-			const users = state.spaces[name].users
-			if (users.length === 0) {
-				return 0
-			} else {
-				return Object.keys(users).length
-			}
-		},
-		sortedSpaces: state => {
-			const sortedSpaces = {}
-			Object.keys(state.spaces).sort().forEach((value, index) => {
-				sortedSpaces[value] = state.spaces[value]
-			})
-			return sortedSpaces
-		},
-	},
+	getters,
 })
--- src/tests/unit/store.js
+++ src/tests/unit/store.js
@@ -1,12 +1,42 @@
+import { curry, mapObjIndexed } from 'ramda'
+import { getters } from '../../../src/store/getters'
 import mutations from '../../../src/store/mutations'
 
+const bindGetterToState = curry((getters, state, num, key) => getters[key](state, getters))
 const expect = require('chai').expect
 
 describe('Vuex store tests', () => {
+	let state
+	let bindGetters
 
-	const state = {
-		spaces: {},
-	}
+	beforeEach(() => {
+		state = {
+			spaces: {
+				'test-space': {
+					color: '#f5f5f5',
+					groups: [],
+					id: '42',
+					isOpen: false,
+					name: 'test-space',
+					quota: 'unlimited',
+					users: [
+						{
+							uid: 1234,
+							name: 'Jane Doe',
+							email: 'jane@acme.org',
+							subtitle: 'jane@acme.org',
+							groups: [
+								'GE-test-space',
+								'yolo-1234',
+								],
+							role: 'admin',
+						},
+					],
+				},
+			},
+		}
+		bindGetters = () => mapObjIndexed(bindGetterToState(getters, state), getters)
+	})
 
 	it('Adds a space in the Vuex store', () => {
 		mutations.addSpace(state, {
@@ -14,12 +44,12 @@ describe('Vuex store tests', () => {
 			groups: [],
 			id: '42',
 			isOpen: false,
-			name: 'test-space',
+			name: 'test-space-2',
 			quota: 'unlimited',
 			users: [],
 		})
 
-		expect(state.spaces['test-space']).not.undefined
+		expect(state.spaces['test-space-2']).not.undefined
 	})
 
 	it('Adds a group to the space', () => {
@@ -64,6 +94,18 @@ describe('Vuex store tests', () => {
 		})
 
 		expect(state.spaces['test-space'].users['John Doe']).not.undefined
+	})
+
+	it('Count users in workspace', () => {
+		const getters = bindGetters()
+		const count = getters.spaceUserCount('test-space')
+		expect(count).equal(1)
+	})
+
+	it('Count users in group', () => {
+		const getters = bindGetters()
+		const count = getters.groupUserCount('test-space', 'GE-test-space')
+		expect(count).equal(1)
 	})
 
 	it('Removes a user from the space', () => {
