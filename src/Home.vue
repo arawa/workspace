04ec9584bdbfceff343f90808d96c75b43e70d80
zak39 <fotia.baptiste@hotmail.com>
2021-06-14 20:12:19 +0200

    fix(Vue): Remove 'index.php' in API's URLs

--- src/Home.vue
+++ src/Home.vue
@@ -71,7 +71,7 @@ export default {
 		Content,
 	},
 	created() {
-		axios.get(generateUrl('/index.php/apps/workspace/spaces'))
+		axios.get(generateUrl('/apps/workspace/spaces'))
 			.then(resp => {
 				Object.values(resp.data).forEach(folder => {
 					this.$store.commit('addSpace', {
@@ -109,7 +109,7 @@ export default {
 				// TODO inform user?
 				return
 			}
-			axios.post(generateUrl('/index.php/apps/workspace/spaces'),
+			axios.post(generateUrl('/apps/workspace/spaces'),
 				{
 					spaceName: name,
 				}
