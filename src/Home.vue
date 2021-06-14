ea263c406a374e51c093e92ca4f57cc8646bf1c7
Cyrille Bollu <cyr.debian@bollu.be>
2021-06-14 14:17:51 +0200

    Uses Object.keys() rather than Object.entries() for parsing groups in Home.vue
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/Home.vue
+++ src/Home.vue
@@ -28,13 +28,13 @@
 					{{ userCount(space) }}
 				</CounterBubble>
 				<div>
-					<AppNavigationItem v-for="group in Object.entries($store.state.spaces[name].groups)"
-						:key="group[0]"
+					<AppNavigationItem v-for="group in Object.keys($store.state.spaces[name].groups)"
+						:key="group"
 						icon="icon-group"
-						:to="{path: `/group/${name}/${group[0]}`}"
-						:title="group[0]">
+						:to="{path: `/group/${name}/${group}`}"
+						:title="group">
 						<CounterBubble slot="counter">
-							{{ groupUserCount(space, group[0]) }}
+							{{ groupUserCount(space, group) }}
 						</CounterBubble>
 					</AppNavigationItem>
 				</div>
