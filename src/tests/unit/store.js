79d3a6e77c0d4f1cd2e5384ae005a6d8cfc0b5a9
Cyrille Bollu <cyr.debian@bollu.be>
2021-06-14 16:43:04 +0200

    Adds unit test for the home component
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/tests/unit/store.js
+++ src/tests/unit/store.js
@@ -49,7 +49,9 @@ describe('Vuex store tests', () => {
 			users: [],
 		})
 
-		expect(state.spaces['test-space-2']).not.undefined
+		/* https://github.com/standard/standard/issues/690#issuecomment-278533482 */
+		/* eslint-disable-next-line no-unused-expressions */
+		expect(state.spaces['test-space']).to.not.be.undefined
 	})
 
 	it('Adds a group to the space', () => {
@@ -67,9 +69,10 @@ describe('Vuex store tests', () => {
 			group: 'test-group',
 		})
 
-		expect(state.spaces['test-space'].groups['test-group']).undefined
+		/* https://github.com/standard/standard/issues/690#issuecomment-278533482 */
+		/* eslint-disable-next-line no-unused-expressions */
+		expect(state.spaces['test-space'].groups['test-group']).to.be.undefined
 	})
-
 
 	it('Sets space quota', () => {
 		mutations.setSpaceQuota(state, {
