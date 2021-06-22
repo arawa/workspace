775a8657495096e125ceb84623f0609ff4c0087f
Cyrille Bollu <cyr.debian@bollu.be>
2021-06-22 12:13:47 +0200

    Adds user notifications for some error cases in vuex store
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/store/index.js
+++ src/store/index.js
@@ -28,16 +28,22 @@ export default new Vuex.Store({
 				.then((resp) => {
 					if (resp.status !== 200) {
 						// Revert action an inform user
-						// TODO Inform user
 						context.commit('addUserToWorkspace', user)
+						this._vm.$notify({
+							title: 'Error',
+							text: 'An error occured while trying to add user ' + user.name + ' to workspace.<br>The error is: ' + resp.statusText,
+							type: 'error',
+						})
 					}
 				}).catch((e) => {
 					// Revert action an inform user
-					// TODO Inform user
 					context.commit('addUserToWorkspace', user)
+					this._vm.$notify({
+						title: 'Network error',
+						text: 'A network error occured while trying to add user ' + user.name + ' to workspace.<br>The error is: ' + e,
+						type: 'error',
+					})
 				})
-			// eslint-disable-next-line no-console
-			console.log('User ' + user.name + ' removed from space ' + name)
 		},
 		// Change a user's role from admin to user (or the opposite way)
 		toggleUserRole(context, { name, user }) {
@@ -59,7 +65,6 @@ export default new Vuex.Store({
 				.then((resp) => {
 					if (resp.status !== 200) {
 						// Revert action an inform user
-						// TODO Inform user
 						if (user.role === 'admin') {
 							user.role = 'user'
 							user.groups.splice(user.groups.indexOf('GE-' + name), 1)
@@ -70,19 +75,26 @@ export default new Vuex.Store({
 							user.groups.push('GE-' + name)
 						}
 						context.commit('updateUser', { name, user })
+						this._vm.$notify({
+							title: 'Error',
+							text: 'An error occured while trying to change the role of user ' + user.name + '.<br>The error is: ' + resp.statusText,
+							type: 'error',
+						})
 					}
 				}).catch((e) => {
 					// Revert action an inform user
-					// TODO Inform user
 					if (user.role === 'admin') {
 						user.role = 'user'
 					} else {
 						user.role = 'admin'
 					}
 					context.commit('updateUser', { name, user })
+					this._vm.$notify({
+						title: 'Network error',
+						text: 'An error occured while trying to change the role of user ' + user.name + '.<br>The error is: ' + e,
+						type: 'error',
+					})
 				})
-			// eslint-disable-next-line no-console
-			console.log('Role of user ' + user.name + ' changed')
 		},
 		updateSpace(context, { space }) {
 			context.commit('updateSpace', space)
@@ -113,18 +125,24 @@ export default new Vuex.Store({
 			const url = generateUrl(`/apps/groupfolders/folders/${name}/quota`)
 			axios.post(url, { quota })
 				.then(resp => {
-					// eslint-disable-next-line
-					console.log('this', this)
-					if (resp.status === 200) {
+					if (resp.status !== 200) {
 						// Reverts change made in the frontend in case of error
-						this.$store.setSpaceQuota(this.$route.params.space, oldQuota)
-						// TODO inform user
+						context.commit('setSpaceQuota', { name, oldQuota })
+						this._vm.$notify({
+							title: 'Error',
+							text: 'An error occured while trying to update the workspace\'s quota.<br>The error is: ' + resp.statusText,
+							type: 'error',
+						})
 					}
 				})
 				.catch((e) => {
 					// Reverts change made in the frontend in case of error
 					context.commit('setSpaceQuota', { name, oldQuota })
-					// TODO Inform user
+					this._vm.$notify({
+						title: 'Network rrror',
+						text: 'A network error occured while trying to update the workspace\'s quota.<br>The error is: ' + e,
+						type: 'error',
+					})
 				})
 		},
 	},
