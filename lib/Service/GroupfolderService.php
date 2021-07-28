b544f177a906e3f180edfc81d70c8d1583879a90
Cyrille Bollu <cyr.debian@bollu.be>
2021-07-28 09:25:39 +0200

    Makes function GroupfolderService::delete return the status code of the response to the groupfolder's API call rather than the response from the HttpClient
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- lib/Service/GroupfolderService.php
+++ lib/Service/GroupfolderService.php
@@ -148,7 +148,7 @@ class GroupfolderService {
 
     /**
      * @param int $folderId the space name to delete.
-     * @return object that is the response from httpClient
+     * @return int The result of the call to the groupfolder API
      * 
     */
     public function delete($folderId) {
@@ -164,14 +164,16 @@ class GroupfolderService {
             ]
         );
 
-        return $response;
+	$resp = json_decode($response->getBody(), true);
+
+	return $resp['ocs']['meta']['statuscode'];
     }
 
     /**
      * Returns a groupfolder's details
      *
      * @param int $folderId
-     * @return object The groupfolder's details
+     * @return array The groupfolder's details
      */
     public function get($folderId) {
         $response = $this->httpClient->get(
