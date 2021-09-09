29d6fa527d11785a4e1855310c323a47d6a47e11
Cyrille Bollu <cyr.debian@bollu.be>
2021-09-09 17:14:31 +0200

    fix bad space name given to isGeneralManager(user, spaceName) getter in SpaceTable
    Signed-off-by: Cyrille Bollu <cyr.debian@bollu.be>

--- src/SpaceTable.vue
+++ src/SpaceTable.vue
@@ -72,7 +72,7 @@ export default {
 		},
 		// Returns all workspace's managers
 		workspaceManagers(space) {
-			return Object.values(space.users).filter((u) => this.$store.getters.isGeneralManager(u, this.$store.getters.space))
+			return Object.values(space.users).filter((u) => this.$store.getters.isGeneralManager(u, space.name))
 		},
 		openSpace(name) {
 			this.$store.state.spaces[name].isOpen = true
