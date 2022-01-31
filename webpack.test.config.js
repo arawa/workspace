6d6abc489efa5cc86648543c8c2c1de385643982
John Molakvoæ <skjnldsv@protonmail.com>
2022-01-31 14:11:09 +0100

    Mochapack testing
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -20,20 +20,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+/* eslint-disable node/no-extraneous-require */
 
 const path = require('path')
 const { merge } = require('webpack-merge')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const nodeExternals = require('webpack-node-externals')
-require('jsdom-global')('', {url: 'http://localhost'})
+
+require('jsdom-global')('', {
+	url: 'http://localhost',
+})
 
 const appName = process.env.npm_package_name
-
 const config = merge(webpackConfig, {
 	mode: 'development',
 	devtool: 'inline-cheap-module-source-map',
 	externals: [nodeExternals()],
-	// Overrides the output config provided by Nextcloud as for some reason 
+	// Overrides the output config provided by Nextcloud as for some reason
 	// contenthash doesn't get appended to filenames
 	output: {
 		path: path.resolve('./js'),
