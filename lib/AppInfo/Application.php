277b8bf05bb4ee5c25ab859b6e8fce7a88618d56
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-01-05 15:37:10 +0100

    chore(PHP): Create the register function
    I created the register function to use the context
    "IRegistrationContext" instead of the containter to register a service.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,18 +1,33 @@
 <?php
 /**
- * @copyright 2021 Arawa <TODO>
+ * @copyright Copyright (c) 2022 Arawa
  *
  * @author 2021 Cyrille Bollu <cyrille@bollu.be>
  * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace OCA\Workspace\AppInfo;
 
 use OCA\Workspace\Middleware\IsGeneralManagerMiddleware;
-use OCA\Workspace\Middleware\WorkspaceAccessControlMiddleware;
 use OCA\Workspace\Middleware\IsSpaceAdminMiddleware;
+use OCA\Workspace\Middleware\WorkspaceAccessControlMiddleware;
 use OCA\Workspace\Service\UserService;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Utility\IControllerMethodReflector;
 use OCP\IRequest;
 use OCP\IURLGenerator;
@@ -23,17 +38,18 @@ class Application extends App {
 
     public function __construct(array $urlParams=[] ) {
         parent::__construct(self::APP_ID, $urlParams);
+    }
 
-        $container = $this->getContainer();
-
-        $container->registerService('WorkspaceAccessControlMiddleware', function($c){
+    public function register(IRegistrationContext $context): void
+    {
+        $context->registerService(WorkspaceAccessControlMiddleware::class, function($c){
             return new WorkspaceAccessControlMiddleware(
                 $c->query(IURLGenerator::class),
                 $c->query(UserService::class)
             );
         });
 
-        $container->registerService('IsSpaceAdminMiddleware', function($c){
+        $context->registerService(IsSpaceAdminMiddleware::class, function($c){
             return new IsSpaceAdminMiddleware(
                 $c->query(IControllerMethodReflector::class),
                 $c->query(IRequest::class),
@@ -41,7 +57,7 @@ class Application extends App {
             );
         });
 
-        $container->registerService('IsGeneralManagerMiddleware', function($c){
+        $context->registerService(IsGeneralManagerMiddleware::class, function($c){
             return new IsGeneralManagerMiddleware(
                 $c->query(IControllerMethodReflector::class),
                 $c->query(IRequest::class),
@@ -49,7 +65,7 @@ class Application extends App {
             );
         });
 
-        $container->registerMiddleware('OCA\Workspace\Middleware\WorkspaceAccessControlMiddleware');
-        $container->registerMiddleware('OCA\Workspace\Middleware\IsSpaceAdminMiddleware');
+        $context->registerMiddleware(WorkspaceAccessControlMiddleware::class);
+        $context->registerMiddleware(IsSpaceAdminMiddleware::class);
     }
 }
