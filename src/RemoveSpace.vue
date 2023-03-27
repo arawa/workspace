3046db8a186f9408c667a217ac6fc3d3813368ac
acdmft <crankyfox@protonmail>
2023-03-27 15:19:42 +0200

    feat(vue): add RemoveSpace component and place it inside NcModal

---
+++ src/RemoveSpace.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="modal__content">
+		<h2>
+			{{ t('workspace', 'Are you sure you want to delete the {spaceName} space ?', { spaceName }) }}
+		</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RemoveSpace',
+	props: {
+		spaceName: { type: String, default: '' },
+	},
+}
+</script>
+<style>
+div.modal__content {
+	display: flex;
+	align-items: center;
+	margin: 0 auto;
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
