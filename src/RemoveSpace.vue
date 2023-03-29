38acbf5a22bae3093ed11f68933f634a2e703aae
acdmft <crankyfox@protonmail>
2023-03-29 10:10:29 +0200

    fix(vue): fix the issue with t() function in RemoveSpace component
    change parametr from spaceName to space

--- src/RemoveSpace.vue
+++ src/RemoveSpace.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="modal__content">
 		<h2>
-			{{ t('workspace', 'Are you sure you want to delete the {spaceName} space ?', { spaceName }) }}
+			{{ t('workspace', 'Are you sure you want to delete the {space} space ?', { space }) }}
 		</h2>
 		<div class="remove-space-actions">
 			<NcButton
@@ -28,6 +28,11 @@ export default {
 	props: {
 		spaceName: { type: String, default: '' },
 	},
+	data() {
+		return {
+			space: this.spaceName,
+		}
+	},
 	methods: {
 		handleCancel() {
 			this.$emit('handle-cancel')
@@ -44,12 +49,12 @@ export default {
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-  width: 600px !important;
+	width: 600px !important;
 }
 .modal__content h2 {
-  font-weight: bold;
-  font-size: 16px;
-  text-align: center;
+	font-weight: bold;
+	font-size: 16px;
+	text-align: center;
 }
 .remove-space-actions {
 	margin-top: 1.5rem;
