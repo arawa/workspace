8117aac884ae36c31645b31896d5d146bd2fb973
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-04-03 16:56:18 +0200

    feature(Vue): Display the content of WorkspaceTab
    I displayed the content of WorkspaceTab if the this.fileInfo.mountType
    is "group" only.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- src/WorkspaceTab.vue
+++ src/WorkspaceTab.vue
@@ -1,21 +1,29 @@
 <template>
-	<div class="content-wsp-sidebar">
+	<div v-if="isGroupfolder"
+		class="content-wsp-sidebar">
 		<h1>Hello</h1>
 		<p class="message">
 			World
 		</p>
 	</div>
+	<NcEmptyContent v-else
+		title="No Workspace or Groupfolder">
+		<p>Sorry, this file is not a Workspace or Groupfolder.</p>
+	</NcEmptyContent>
 </template>
 
 <script>
+import NcEmptyContent from '@nextcloud/vue/dist/Components/NcEmptyContent.js'
 
 export default {
 	name: 'WorkspaceTab',
 	components: {
+		NcEmptyContent,
 	},
 	data() {
 		return {
 			fileInfo: {},
+			isGroupfolder: false,
 		}
 	},
 	created() {
@@ -36,6 +44,23 @@ export default {
 		 */
 		async update(fileInfo) {
 			this.fileInfo = fileInfo
+			// Ici, je peux faire un appel API pour savoir si le fichier est
+			// un groupfolder ou non.
+			if (this.fileInfo.mountType !== 'group') {
+				this.isGroupfolder = false
+				return
+			}
+			console.debug('WorkspaceTab - call the toggleIsGroupfolder function')
+			this.toggleIsGroupfolder()
+			console.debug('WorkspaceTab - this.fileInfo', {
+				name: this.fileInfo.name,
+				mountType: this.fileInfo.mountType,
+				id: this.fileInfo.id,
+			})
+		},
+		toggleIsGroupfolder() {
+			this.isGroupfolder = !this.isGroupfolder
+			console.debug('WorkspaceTab - toggleIsGroupfolder', this.isGroupfolder)
 		},
 	},
 }
