b21b9b3aff64019e99b1b069081efb26c90e9d83
acdmft <crankyfox@protonmail>
2023-04-24 15:40:54 +0200

    style(js): clean code - remove commented lines

--- src/store/actions.js
+++ src/store/actions.js
@@ -109,11 +109,6 @@ export default {
 			})
 			.catch((e) => {
 				context.commit('removeGroupFromSpace', { name, gid })
-				// this._vm.$notify({
-				// 	title: t('workspace', 'Network error'),
-				// 	text: t('workspace', 'A network error occured while trying to create group ') + gid + t('workspace', '<br>The error is: ') + e,
-				// 	type: 'error',
-				// })
         const text = t('workspace', 'A network error occured while trying to create group ') + gid + t('workspace', '<br>The error is: ') + e
         showNotificationError('Network error', text, 5000)
 			})
