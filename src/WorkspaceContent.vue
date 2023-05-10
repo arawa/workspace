6f78f0095266adb592c6c4b7c7fa3d05dcc3ea98
acdmft <crankyfox@protonmail>
2023-05-10 17:22:54 +0200

    feat(vue): add template part in the WorkspaceContent and LeftSidebar components

--- src/WorkspaceContent.vue
+++ src/WorkspaceContent.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Workspace content</h1>
+  <NcAppContent>
+			<NcAppContentDetails>
+				<div
+					v-if="$store.state.loading"
+					class="lds-ring">
+					<div /><div /><div /><div />
+				</div>
+				<div v-else class="workspace-content">
+					<router-view />
+				</div>
+			</NcAppContentDetails>
+		</NcAppContent>
 </template>
 
 <script>
