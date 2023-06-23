62208c960419699a1e8d2a53a448030b6d5da382
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-06-23 12:22:57 +0200

    chore(): Run composer run cs:fix
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Migration/FixMigrationToV300.php
+++ lib/Migration/FixMigrationToV300.php
@@ -2,42 +2,38 @@
 
 namespace OCA\Workspace\Migration;
 
-use OCP\IAppConfig;
-use OCP\Migration\IOutput;
-use Psr\Log\LoggerInterface;
-use OCP\Migration\IRepairStep;
-use OCA\Workspace\Upgrade\Upgrade;
 use OCA\Workspace\AppInfo\Application;
+use OCA\Workspace\Upgrade\Upgrade;
 use OCA\Workspace\Upgrade\UpgradeFixV300V301;
 use OCP\AppFramework\Services\IAppConfig as ServiceIAppConfig;
+use OCP\IAppConfig;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+use Psr\Log\LoggerInterface;
 
-class FixMigrationToV300 implements IRepairStep
-{
+class FixMigrationToV300 implements IRepairStep {
 
-    public function __construct(private ServiceIAppConfig $appConfig,
-        private UpgradeFixV300V301 $upgrade,
-        private IAppConfig $appConfigManager,
-        private LoggerInterface $logger)
-    {
+	public function __construct(private ServiceIAppConfig $appConfig,
+		private UpgradeFixV300V301 $upgrade,
+		private IAppConfig $appConfigManager,
+		private LoggerInterface $logger) {
 		$this->logger->debug('FixMigrationToV300 repair step initialised');
 
-    }
-    public function getName(): string
-    {
-        return 'Try to fix one problem to trigger the migration to v3.0.0 or 3.0.1';
-    }
+	}
+	public function getName(): string {
+		return 'Try to fix one problem to trigger the migration to v3.0.0 or 3.0.1';
+	}
 
-    public function run(IOutput $output): void
-    {
-        $versionInstalled = $this->appConfig->getAppValue('installed_version');
-        $isBetween300And301 = version_compare($versionInstalled, '3.0.0', '>=') && version_compare($versionInstalled, '3.0.2', '<');
-        if (!$isBetween300And301) {
-            return;
-        }
+	public function run(IOutput $output): void {
+		$versionInstalled = $this->appConfig->getAppValue('installed_version');
+		$isBetween300And301 = version_compare($versionInstalled, '3.0.0', '>=') && version_compare($versionInstalled, '3.0.2', '<');
+		if (!$isBetween300And301) {
+			return;
+		}
 
-        if (!$this->appConfigManager->hasKey(Application::APP_ID, Upgrade::CONTROL_MIGRATION_V3)) {
-            $this->appConfig->setAppValue(Upgrade::CONTROL_MIGRATION_V3, '0');
-        }
+		if (!$this->appConfigManager->hasKey(Application::APP_ID, Upgrade::CONTROL_MIGRATION_V3)) {
+			$this->appConfig->setAppValue(Upgrade::CONTROL_MIGRATION_V3, '0');
+		}
 
 		if (!$this->appConfigManager->hasKey(Application::APP_ID, 'DISPLAY_PREFIX_MANAGER_GROUP')
 			&& !$this->appConfigManager->hasKey(Application::APP_ID, 'DISPLAY_PREFIX_USER_GROUP')) {
@@ -45,6 +41,6 @@ class FixMigrationToV300 implements IRepairStep
 			$this->appConfig->setAppValue('DISPLAY_PREFIX_USER_GROUP', 'U-');
 		}
 
-        $this->upgrade->upgrade();
-    }
+		$this->upgrade->upgrade();
+	}
 }
