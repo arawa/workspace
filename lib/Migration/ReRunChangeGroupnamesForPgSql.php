75609119a9b2c31100d092bf7f53197e377014d6
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-09-11 19:07:49 +0200

    feat(php): Rerun the change groupnames for pgsql
    Rerun the change groupnames for pgsql when the control migration is to
    false.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

---
+++ lib/Migration/ReRunChangeGroupnamesForPgSql.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OCA\Workspace\Migration;
+
+use OCP\IConfig;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+use OCA\Workspace\Upgrade\Upgrade;
+use OCA\Workspace\Upgrade\UpgradeV300;
+use OCP\AppFramework\Services\IAppConfig;
+
+class ReRunChangeGroupnamesForPgSql implements IRepairStep
+{
+    public function __construct(
+        private IConfig $config,
+        private IAppConfig $appConfig,
+        private UpgradeV300 $upgrade,
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return 'Rerun the change groupnames repair step for a Nextcloud instance using PostgreSQL.';
+    }
+
+    public function run(IOutput $output): void
+    {
+        $sgbdName = $this->config->getSystemValue('dbtype');
+
+        if ($sgbdName !== 'pgsql')
+        {
+            return;
+        }
+
+        $statusMigration = boolval($this->appConfig->getAppValue(Upgrade::CONTROL_MIGRATION_V3, '1'));
+
+        if ($statusMigration === true)
+        {
+            return;
+        }
+
+        $this->upgrade->upgrade();
+    }
+}
