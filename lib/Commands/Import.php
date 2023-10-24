7903d173f4333f5989f58adfd51effc724a7f4a1
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-10-24 15:23:25 +0200

    feature(php): Create the Import command
    I created the "Import" class that is a Workspace command for Nextcloud.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

---
+++ lib/Commands/Import.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace OCA\Workspace\Commands;
+
+use OCA\Workspace\Files\CsvMassCreatingWorkspaces;
+use OCA\Workspace\Group\Admin\AdminGroup;
+use OCA\Workspace\Group\Admin\AdminGroupManager;
+use OCA\Workspace\Service\WorkspaceService;
+use OCA\Workspace\Space\SpaceManager;
+use OCP\IGroupManager;
+use OCP\IRequest;
+use OCP\IUserManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Import extends Command {
+
+	public function __construct(
+		private IGroupManager $groupManager,
+		private IRequest $request,
+		private IUserManager $userManager,
+		private CsvMassCreatingWorkspaces $csvCreatingWorkspaces,
+		private SpaceManager $spaceManager,
+		private AdminGroup $adminGroup,
+		private WorkspaceService $workspaceService) {
+		parent::__construct();
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$path = realpath($input->getArgument('path'));
+	  
+		if (!$this->csvCreatingWorkspaces->isCsvFile($path)) {
+			// throw new Exception("It's not a csv file.");
+			throw new \Exception("It's not a csv file. Your file is a " . (string)$this->csvCreatingWorkspaces->getMimeType($path) . " mimetype.");
+		}
+
+		if (!$this->csvCreatingWorkspaces->hasProperHeader($path)) {
+			throw new \Exception('No respect the glossary headers.');
+		}
+
+		$dataFormated = $this->csvCreatingWorkspaces->parser($path);
+
+		foreach ($dataFormated as $data) {
+
+			$users = $this->workspaceService->searchUsers($data['user_uid']);
+			$user = $users[0];
+
+			$workspace = $this->spaceManager->create($data['workspace_name']);
+			$groupname = AdminGroupManager::findWorkspaceManager($workspace);
+			$this->adminGroup->addUser($user, $groupname);
+		}
+
+		return 0;
+	}
+
+	protected function configure(): void {
+		$this
+			->setName('workspace:import')
+			->setDescription('This command allows you to import a csv file to create workspaces and define the workspace manager users.')
+			->addArgument('path', InputArgument::REQUIRED, 'The path of the csv file.');
+		parent::configure();
+	}
+}
