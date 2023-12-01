424ee2a8e6206b3fe55aada2bcf350dacb510eda
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-12-01 18:32:20 +0100

    chore(): Deprecated for the formatUser function
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Service/UserService.php
+++ lib/Service/UserService.php
@@ -44,6 +44,9 @@ class UserService {
 	 *
 	 * Given a IUser, returns an array containing all the user information
 	 * needed for the frontend
+     * 
+     * @deprecated 3.1.0|4.0.0
+     * @uses OCA\Workspace\Users\UserFormatter
 	 *
 	 * @param IUser $user
 	 * @param array $space
@@ -52,7 +55,6 @@ class UserService {
 	 * @return array|null
 	 *
 	 */
-
 	public function formatUser(IUser $user, array $space, string $role): array|null {
 		if (is_null($user)) {
 			return null;
