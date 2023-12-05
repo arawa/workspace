d139fbba4c97495cbf9b7c998751fccb73aa31a9
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-12-05 18:21:26 +0100

    refactor(): Read a csv file is faster than before
    Now, we use CsvReader as a function in a loop (for example).
    CsvReader use __invoke and returns a Generator.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Files/Csv/ImportUsers/Parser.php
+++ lib/Files/Csv/ImportUsers/Parser.php
@@ -3,31 +3,36 @@
 namespace OCA\Workspace\Files\Csv\ImportUsers;
 
 use OCA\Workspace\Files\Csv\CsvParserInterface;
+use OCA\Workspace\Files\Csv\CsvReader;
 use OCA\Workspace\Files\Csv\ImportUsers\Header;
 use OCA\Workspace\Files\ManagerConnectionFileInterface;
+use OCA\Workspace\Users\Formatter\UserImportedFormatter;
 
 class Parser implements CsvParserInterface
 {
 
+    /**
+     * @return UserImportedFormatter[]
+     */
     public function parser(ManagerConnectionFileInterface $file): array {
-		$handle = $file->open();
-		
-		if ($handle === false) {
-			throw new \Exception("Imposible to open the $file->getPath() file.");
-		}
-
 		$users = [];
-		$tableHeader = fgetcsv($handle, 1000, ","); // ignore the first line
-		$tableHeader = array_map('strtolower', $tableHeader);
 
-		$nameIndex = HeaderExtractor::getIndex(Header::DISPLAY_NAME, $tableHeader);
-		$roleIndex = HeaderExtractor::getIndex(Header::ROLE, $tableHeader);
-
-		while (($data = fgetcsv($handle, 1000, ",")) !== false) {
-			$users[] = ['name' => $data[$nameIndex], 'role' => $data[$roleIndex]];
-		}
-		$file->close();
+        foreach ((new CsvReader)($file) as $data ) {
+            $keys = array_keys($data);
+            $uid = $this->getKey($keys, Header::DISPLAY_NAME);
+            $role = $this->getKey($keys, Header::ROLE);
+            $users[] = new UserImportedFormatter($data[$uid], $data[$role]);
+        }
 
 		return $users;
 	}
+
+    private function getKey(array $needle, array $haystack): string {
+        return array_values(
+            array_filter(
+                $needle,
+                fn($key) => in_array($key, $haystack)
+            )
+        )[0];
+    }
 }
---
+++ lib/Users/Formatter/UserImportedFormatter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OCA\Workspace\Users\Formatter;
+
+class UserImportedFormatter {
+    public function __construct(
+        public string $uid,
+        public string $role
+    )
+    {
+    }
+}
