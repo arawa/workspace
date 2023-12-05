3062f8311a4369ba2c7667726c884416b1e685b5
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-12-05 18:21:26 +0100

    refactor(): Read a csv file is faster than before
    Now, we use CsvReader as a function in a loop (for example).
    CsvReader use __invoke and returns a Generator.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Files/Csv/ImportUsers/Parser.php
+++ lib/Files/Csv/ImportUsers/Parser.php
@@ -19,8 +19,10 @@ use OCA\Workspace\Files\ManagerConnectionFileInterface;
 =======
 use OCA\Workspace\Files\Csv\CsvReader;
 use OCA\Workspace\Files\Csv\CsvParserInterface;
+use OCA\Workspace\Files\Csv\CsvReader;
 use OCA\Workspace\Files\Csv\ImportUsers\Header;
 use OCA\Workspace\Files\ManagerConnectionFileInterface;
+<<<<<<< HEAD
 use OCA\Workspace\Files\Csv\ImportUsers\HeaderExtractor;
 >>>>>>> b5211d5 (refactor(php): Improvement in reading speed)
 use OCA\Workspace\Users\Formatter\UserImportedFormatter;
@@ -62,7 +64,35 @@ class Parser implements CsvParserInterface {
             $users[] = new UserImportedFormatter($data[$uid], $data[$role]);
         }
 >>>>>>> b5211d5 (refactor(php): Improvement in reading speed)
+=======
+use OCA\Workspace\Users\Formatter\UserImportedFormatter;
+
+class Parser implements CsvParserInterface
+{
+
+    /**
+     * @return UserImportedFormatter[]
+     */
+    public function parser(ManagerConnectionFileInterface $file): array {
+		$users = [];
+
+        foreach ((new CsvReader)($file) as $data ) {
+            $keys = array_keys($data);
+            $uid = $this->getKey($keys, Header::DISPLAY_NAME);
+            $role = $this->getKey($keys, Header::ROLE);
+            $users[] = new UserImportedFormatter($data[$uid], $data[$role]);
+        }
+>>>>>>> 68bb094 (refactor(): Read a csv file is faster than before)
 
 		return $users;
 	}
+
+    private function getKey(array $needle, array $haystack): string {
+        return array_values(
+            array_filter(
+                $needle,
+                fn($key) => in_array($key, $haystack)
+            )
+        )[0];
+    }
 }
