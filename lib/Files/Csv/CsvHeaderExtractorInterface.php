c324c1677c2bbcc54d8d0d71f6f460a39750bcb0
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-12-06 16:29:43 +0100

    refactor(php): Improvement in reading speed
    I impoved the reading speed and the extracting of the header name.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Files/Csv/CsvHeaderExtractorInterface.php
+++ lib/Files/Csv/CsvHeaderExtractorInterface.php
@@ -5,4 +5,5 @@ namespace OCA\Workspace\Files\Csv;
 interface CsvHeaderExtractorInterface
 {
     public static function getIndex(array $haystack, array $needles): int|bool;
+    public static function getHeaderName(array $needles, array $haystack): string;
 }
