e485029c05fcb13dc0faa681b10a6176023af322
Baptiste Fotia <fotia.baptiste@hotmail.com>
2024-01-22 10:53:42 +0100

    refactor(vue):Create tabs and split the components
    I created tabs to choice between a manual select or import by csv files.
    
    I took advantage of the opportunity to split the components and reduce
    the size of the code.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

---
+++ src/ButtonUploadShareFiles.vue
@@ -0,0 +1,127 @@
+<!--
+	@copyright Copyright (c) 2017 Arawa
+
+	@author 2024 Baptiste Fotia <baptiste.fotia@arawa.fr>
+
+	@license GNU AGPL version 3 or any later version
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+	<div>
+		<NcButton @click="shareCsvFromFiles()">
+			<template #icon>
+				<Folder :size="20" />
+			</template>
+			{{ t('workspace', 'Choose from Files') }}
+		</NcButton>
+	</div>
+</template>
+
+<script>
+import { getFilePickerBuilder } from '@nextcloud/dialogs'
+import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import showNotificationError from './services/Notifications/NotificationError.js'
+import Folder from 'vue-material-design-icons/Folder.vue'
+
+const picker = getFilePickerBuilder(t('deck', 'File to share'))
+	.setMultiSelect(false)
+	.setModal(true)
+	.setType(1)
+	.allowDirectories()
+	.build()
+
+export default {
+	name: 'ButtonUploadShareFiles',
+	components: {
+		NcButton,
+		Folder,
+	},
+	props: {
+		allSelectedUsers: {
+			type: Array,
+			default: () => [],
+		},
+	},
+	methods: {
+		shareCsvFromFiles() {
+			picker.pick()
+				.then(async (path, title) => {
+					console.debug(`path ${path} selected for sharing, title ${title}`)
+					const space = this.$store.state.spaces[this.$route.params.space]
+					const spaceString = JSON.stringify(space)
+					const bodyFormData = new FormData()
+					bodyFormData.append('path', path)
+					bodyFormData.append('space', spaceString)
+					try {
+						const users = await this.$store.dispatch('importCsvFromFiles', { formData: bodyFormData })
+						let usersToDisplay = this.filterAlreadyPresentUsers(users)
+						usersToDisplay = this.addSubtitleToUsers(usersToDisplay)
+						// this.allSelectedUsers = [...this.allSelectedUsers, ...usersToDisplay]
+						this.$emit('push', [...this.allSelectedUsers, ...usersToDisplay])
+					} catch (err) {
+						let duration = 5000
+
+						// change the duration of the notification
+						// related to the number of the word.
+						if (err.response.data.data.message.split(' ').length >= 30) {
+							duration = 8000
+						}
+
+						const title = err.response.data.data.title
+						const text = err.response.data.data.message
+						showNotificationError(title, text, duration)
+						console.error(err.response.data.exception)
+					}
+				})
+		},
+		// When adding users to a space, show only those users who are not already member of the space
+		filterAlreadyPresentUsers(recvUsers) {
+			let users = []
+			console.error('par là 1')
+			if (this.$route.params.group === undefined) {
+				console.error('par là 2')
+				const space = this.$store.state.spaces[this.$route.params.space]
+				users = recvUsers.filter(user => {
+					console.error('par là 3')
+					return (!(user.uid in space.users))
+				}, space)
+			} else {
+				console.error('par là 4')
+				users = recvUsers.filter(user => {
+					console.error('par là 5')
+					return (!(user.groups.includes(this.$route.params.group)))
+				})
+			}
+			// Filters user that are already selected
+			return users.filter(newUser => {
+				console.error('par là 6')
+				return this.allSelectedUsers.every(user => {
+					return newUser.uid !== user.uid
+				})
+			})
+		},
+		addSubtitleToUsers(users) {
+			return users.map(user => {
+				return {
+					...user,
+					subtitle: user.subtitle ?? '',
+				}
+			})
+		},
+	},
+}
+
+</script>
---
+++ src/UserCard.vue
@@ -0,0 +1,109 @@
+<!--
+	@copyright Copyright (c) 2017 Arawa
+
+	@author 2024 Baptiste Fotia <baptiste.fotia@arawa.fr>
+
+	@license GNU AGPL version 3 or any later version
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+	<div class="user-card">
+		<div class="avatar">
+			<div class="icon-member" :class="$store.getters.isMember($route.params.space, user) ? 'is-member' : ''" />
+			<NcAvatar :display-name="user.name" :user="user.uid" />
+			<div class="user-name">
+				<span> {{ user.name }} </span>
+			</div>
+		</div>
+		<div class="user-entry-actions">
+			<div v-if="!$store.getters.isGEorUGroup($route.params.space, $route.params.group)">
+				<NcCheckboxRadioSwitch type="checkbox"
+					class="role-toggle"
+					:checked="user.role === 'admin'"
+					@update:checked="toggleUserRole(user)">
+					{{ t('workspace', 'S.A.') }}
+				</NcCheckboxRadioSwitch>
+			</div>
+			<NcActions>
+				<NcActionButton icon="icon-delete"
+					@click="removeUserFromBatch(user)">
+					{{ t('workspace', 'remove users from selection') }}
+				</NcActionButton>
+			</NcActions>
+		</div>
+	</div>
+</template>
+
+<script>
+import NcCheckboxRadioSwitch from '@nextcloud/vue/dist/Components/NcCheckboxRadioSwitch.js'
+import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton.js'
+import NcActions from '@nextcloud/vue/dist/Components/NcActions.js'
+import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
+
+export default {
+	name: 'UserCard',
+	components: {
+		NcAvatar,
+		NcActions,
+		NcActionButton,
+		NcCheckboxRadioSwitch,
+	},
+	props: {
+		user: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	methods: {
+		toggleUserRole(user) {
+			this.$emit('toggle-role', user)
+		},
+		removeUserFromBatch(user) {
+			this.$emit('remove-user', user)
+		},
+	},
+}
+</script>
+
+<style scoped>
+
+.user-card {
+  display: flex;
+}
+
+.user-name {
+	margin-left: 10px;
+	max-width: 440px;
+}
+
+.is-member {
+	background-image: url('../img/member.png');
+	background-repeat: no-repeat;
+	background-position: center center;
+	background-size: contain;
+}
+
+.avatar {
+	align-items: center;
+	display: flex;
+	flex-flow: row;
+}
+
+.user-entry-actions {
+  display: flex;
+}
+
+</style>
