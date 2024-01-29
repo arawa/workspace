84f18dac675311e5bf45bccd13fbfe75c4cff9a6
Baptiste Fotia <fotia.baptiste@hotmail.com>
2024-01-29 16:22:14 +0100

    chore(php): Apply composer run cs:fix
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Files/Csv/ImportUsers/Header.php
+++ lib/Files/Csv/ImportUsers/Header.php
@@ -4,15 +4,15 @@ namespace OCA\Workspace\Files\Csv\ImportUsers;
 
 class Header {
 	public const DISPLAY_NAME = [
-        "username",
-        "displayname",
-        "name",
-        "user"
-    ];
+		"username",
+		"displayname",
+		"name",
+		"user"
+	];
 	
-    public const ROLE = [
-        "role",
-        "status",
-        "userrole"
-    ];
+	public const ROLE = [
+		"role",
+		"status",
+		"userrole"
+	];
 }
