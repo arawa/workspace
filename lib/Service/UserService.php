1e92f3566109326a7b3805923e1504717184dec8
Baptiste Fotia <fotia.baptiste@hotmail.com>
2024-03-21 15:57:58 +0100

    refactor(php): Reducing deep nesting
    It's easier to read.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- lib/Service/UserService.php
+++ lib/Service/UserService.php
@@ -60,13 +60,20 @@ class UserService {
 
 		// Gets the workspace subgroups the user is member of
 		$groups = [];
-		if (isset($space['groups']) && is_array($space['groups'])) {
-			foreach ($this->groupManager->getUserGroups($user) as $group) {
-				if (in_array($group->getGID(), array_keys($space['groups']))) {
-					array_push($groups, $group->getGID());
-				}
-			}
-		}
+
+        if (!isset($space['groups'])) {
+            throw new \Exception('The "groups" key is not presetn');
+        }
+
+        if (!is_array($space['groups'])) {
+            throw new \Exception('The "groups" key is not an array');
+        }
+
+        foreach ($this->groupManager->getUserGroups($user) as $group) {
+            if (in_array($group->getGID(), array_keys($space['groups']))) {
+                array_push($groups, $group->getGID());
+            }
+        }
 
 		// Returns a user that is valid for the frontend
 		return [
