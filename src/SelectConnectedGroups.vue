393f20175a21004f68b13297bf40b5df07fa56b6
zak39 <fotia.baptiste@hotmail.com>
2024-06-21 12:21:13 +0200

    style(vue): Select groups to add them later
    We can select groups and list them, then add them to the workspace.

--- src/SelectConnectedGroups.vue
+++ src/SelectConnectedGroups.vue
@@ -12,19 +12,39 @@
 				limit="10"
 				:options="groupsSelectable"
 				:placeholder="t('workspace', 'Start typing text to search for groups')"
+				@option:selected="addGroupToBatch"
 				@search="lookupGroups"
 				@close="groupsSelectable=[]" />
 		</div>
 		<div class="content-group-list">
 			<div v-if="groupsSelected.length !== 0"
 				class="select-group-list">
-				<p>test</p>
+				<div class="group-item"
+					v-for="group in groupsSelected">
+					<div class="group-avatar">
+						<NcAvatar
+							:display-name="group.displayName"
+							:is-no-user="true" />
+						<div class="groupname">
+							<span>{{ group.displayName }}</span>
+						</div>
+					</div>
+					<div>
+						<NcActions>
+							<NcActionButton icon="icon-delete"
+								@click="removeGroupFromBatch(group)">
+								{{ t('workspace', 'remove group from selection') }}
+							</NcActionButton>
+						</NcActions>
+					</div>
+				</div>
 			</div>
 			<NcEmptyContent v-else
 				class="content-group-list-empty"
 				:title="t('workspace', 'No group selected')" />
 		</div>
 		<NcButton type="secondary"
+			@click="validate"
 			class="btn-add-groups">
 			{{ t('workspace', 'Add') }}
 		</NcButton>
@@ -33,6 +53,9 @@
 
 <script>
 import axios from '@nextcloud/axios'
+import NcActions from '@nextcloud/vue/dist/Components/NcActions.js'
+import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton.js'
+import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import NcSelect from '@nextcloud/vue/dist/Components/NcSelect.js'
 import NcEmptyContent from '@nextcloud/vue/dist/Components/NcEmptyContent.js'
@@ -41,6 +64,9 @@ import { generateUrl } from '@nextcloud/router'
 export default {
 	name: 'SelectConnectedGroups',
 	components: {
+		NcActions,
+		NcActionButton,
+		NcAvatar,
 		NcButton,
 		NcEmptyContent,
 		NcSelect,
@@ -62,6 +88,14 @@ export default {
 		}
 	},
 	methods: {
+		addGroupToBatch(group) {
+			this.groupsSelected.push(group)
+		},
+		removeGroupFromBatch(group) {
+			this.groupsSelected = this.groupsSelected.filter((g) => {
+				return g.displayName !== group.displayName
+			})
+		},
 		lookupGroups(term) {
 			if (term === undefined || term === '') {
 				return
@@ -74,17 +108,23 @@ export default {
 				},
 			})
 				.then(response => {
-					const groups = []
+					let groups = []
 
 					for (const key in response.data) {
 						groups.push(response.data[key])
 					}
+					
+					const groupnames = this.groupsSelected.map((group) => group.displayName)
+					groups = groups.filter((group) => !groupnames.includes(group.displayName))
 
 					this.groupsSelectable = groups
 				})
 				.catch(reason => {
 					console.error(reason.message)
 				})
+		},
+		validate() {
+			console.debug('To add these groups to the workspace', this.groupsSelected)
 		},
 	},
 }
@@ -140,4 +180,27 @@ export default {
 .btn-add-groups {
 	margin-top: 8px;
 }
+
+.groupname {
+	margin-left: 14px;
+}
+
+.select-group-list {
+	display: flex;
+	flex-direction: column;
+	overflow: scroll;
+	height: 100%;
+}
+
+.group-item {
+	display: flex;
+	justify-content: space-between;
+
+}
+
+.group-avatar {
+	display: flex;
+	align-items: center;
+}
+
 </style>
