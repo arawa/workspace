7a758ee32716e322f8ada005db7c85e223f6e756
zak39 <fotia.baptiste@hotmail.com>
2024-08-02 10:56:12 +0200

    feat(capabilities): get the groupfolders version

--- lib/AppInfo/Capabilities.php
+++ lib/AppInfo/Capabilities.php
@@ -6,11 +6,14 @@ namespace OCA\Workspace\AppInfo;
 
 use OCP\App\IAppManager;
 use OCP\Capabilities\ICapability;
+use OCP\IAppConfig;
 
 class Capabilities implements ICapability
 {
-    public function __construct(private IAppManager $appManager)
-    {
+    public function __construct(
+        private IAppManager $appManager,
+        private IAppConfig $appConfigManager
+    ) {
     }
 
     public function getCapabilities(): array
@@ -18,7 +21,8 @@ class Capabilities implements ICapability
         return [
             Application::APP_ID => [
                 'version' => $this->appManager->getAppVersion(Application::APP_ID),
-                'is_enabled' => $this->appManager->isEnabledForUser(Application::APP_ID)
+                'is_enabled' => $this->appManager->isEnabledForUser(Application::APP_ID),
+                'groupfolders_version' => $this->appManager->getAppVersion('groupfolders'),
             ]
         ];
     }
