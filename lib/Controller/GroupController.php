887dc1e87e622e928dcdf95b797491cd0e36e751
zak39 <fotia.baptiste@hotmai.com>
2024-10-02 17:55:20 +0200

    feat(controller): Initialize user count to 0
    When creating a group, the usersCount is initialized to 0 for the new
    group.

--- lib/Controller/GroupController.php
+++ lib/Controller/GroupController.php
@@ -103,7 +103,8 @@ class GroupController extends Controller {
 		return new JSONResponse([
 			'group' => [
 				'gid' => $NCGroup->getGID(),
-				'displayName' => $NCGroup->getDisplayName()
+				'displayName' => $NCGroup->getDisplayName(),
+                'usersCount' => 0,
 			]
 		]);
 	}
