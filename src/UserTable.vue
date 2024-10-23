555b9872da25165bab6f8dd0fa54b74d8b79c259
zak39 <fotia.baptiste@hotmai.com>
2024-10-23 15:12:58 +0200

    style(vue): Fix indentation with 4 tabs

--- src/UserTable.vue
+++ src/UserTable.vue
@@ -1,23 +1,23 @@
 <!--
-  @copyright Copyright (c) 2017 Arawa
+	@copyright Copyright (c) 2017 Arawa
 
-  @author 2021 Baptiste Fotia <baptiste.fotia@arawa.fr>
-  @author 2021 Cyrille Bollu <cyrille@bollu.be>
+	@author 2021 Baptiste Fotia <baptiste.fotia@arawa.fr>
+	@author 2021 Cyrille Bollu <cyrille@bollu.be>
 
-  @license GNU AGPL version 3 or any later version
+	@license GNU AGPL version 3 or any later version
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as
-  published by the Free Software Foundation, either version 3 of the
-  License, or (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Affero General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -140,34 +140,34 @@ export default {
 		},
 	},
 	methods: {
-    sortGroups(groups) {
-      const spacename = this.$route.params.space
-      const groupsSorted = this.sortedGroups([...groups], spacename)
-      return groupsSorted.map(group => this.$store.getters.groupName(spacename, group)).join(', ')
-    },
-    sortedGroups(groups, spacename) {
-      groups.sort((groupCurrent, groupNext) => {
-        // Makes sure the GE- group is first in the list
-        // These tests must happen before the tests for the U- group
-        const GEGroup = this.$store.getters.GEGroup(spacename)
-        if (groupCurrent === GEGroup) {
-          return -1
-        }
-        if (groupNext === GEGroup) {
-          return 1
-        }
-        // Makes sure the U- group is second in the list
-        // These tests must be done after the tests for the GE- group
-        const UGroup = this.$store.getters.UGroup(spacename)
-        if (groupCurrent === UGroup) {
-          return -1
-        }
-        if (groupNext === UGroup) {
-          return 1
-        }
-      })
-      return groups
-    },
+		sortGroups(groups) {
+			const spacename = this.$route.params.space
+			const groupsSorted = this.sortedGroups([...groups], spacename)
+			return groupsSorted.map(group => this.$store.getters.groupName(spacename, group)).join(', ')
+		},
+		sortedGroups(groups, spacename) {
+			groups.sort((groupCurrent, groupNext) => {
+				// Makes sure the GE- group is first in the list
+				// These tests must happen before the tests for the U- group
+				const GEGroup = this.$store.getters.GEGroup(spacename)
+				if (groupCurrent === GEGroup) {
+					return -1
+				}
+				if (groupNext === GEGroup) {
+					return 1
+				}
+				// Makes sure the U- group is second in the list
+				// These tests must be done after the tests for the GE- group
+				const UGroup = this.$store.getters.UGroup(spacename)
+				if (groupCurrent === UGroup) {
+					return -1
+				}
+				if (groupNext === UGroup) {
+					return 1
+				}
+			})
+			return groups
+		},
 		// Removes a user from a workspace
 		deleteUser(user) {
 			const space = this.$store.state.spaces[this.$route.params.space]
@@ -215,11 +215,11 @@ export default {
 }
 
 .user-admin:hover {
-  background-color: #f5f5f5 !important;
+	background-color: #f5f5f5 !important;
 }
 
 .user-simple:hover {
-  background-color: #f5f5f5 !important;
+	background-color: #f5f5f5 !important;
 }
 
 .user-name {
