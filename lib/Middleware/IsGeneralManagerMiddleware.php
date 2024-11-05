f5e814c811467752b827357ac64424a9d8fa773b
zak39 <fotia.baptiste@hotmai.com>
2024-11-05 18:07:07 +0100

    style: composer run cs:fix

--- lib/Middleware/IsGeneralManagerMiddleware.php
+++ lib/Middleware/IsGeneralManagerMiddleware.php
@@ -34,7 +34,6 @@ use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Middleware;
 use OCP\AppFramework\Utility\IControllerMethodReflector;
 use OCP\IRequest;
-use PHPUnit\Util\Json;
 
 class IsGeneralManagerMiddleware extends Middleware {
 	public function __construct(
