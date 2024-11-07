a09b101db53ec4bcf64f3c05c35025a83090122b
zak39 <fotia.baptiste@hotmai.com>
2024-11-07 16:04:11 +0100

    style(js): Indent with tabs

--- src/services/spaceService.js
+++ src/services/spaceService.js
@@ -173,13 +173,13 @@ export function renameSpace(spaceId, newSpaceName) {
 			return respFormat
 		})
 		.catch(error => {
-      if ('response' in error && 'data' in error.response) {
+			if ('response' in error && 'data' in error.response) {
 				showNotificationError(error.response.data.title, error.response.data.message, 5000)
-        throw new Error(error.response.data.message)
+				throw new Error(error.response.data.message)
 			} else {
 				showNotificationError('Error to rename a workspace', error.message, 5000)
-        console.error('Problem to rename the space', error)
-        throw new Error(error.message)
+				console.error('Problem to rename the space', error)
+				throw new Error(error.message)
 			}
 		})
 
