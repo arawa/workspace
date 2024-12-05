4537f76d85cb0767cdcb9816063143c6dc3c7d46
zak39 <fotia.baptiste@hotmai.com>
2024-12-05 16:34:04 +0100

    style: run composer run cs:fix

--- lib/Files/Csv/StructureValidator.php
+++ lib/Files/Csv/StructureValidator.php
@@ -5,18 +5,17 @@ namespace OCA\Workspace\Files\Csv;
 use OCA\Workspace\Files\Csv\ImportUsers\Header;
 use OCA\Workspace\Files\FileInterface;
 
-class StructureValidator
-{
-    
-    public static function checkCommaAllLines(FileInterface $file): bool {
+class StructureValidator {
+	
+	public static function checkCommaAllLines(FileInterface $file): bool {
 		$handle = $file->open();
-        $nbFieldsRequired = count(Header::FIELDS_REQUIRED);
-        while(($data = fgetcsv($handle, 1000, Separator::COMMA)) !== false) {
-            if (count($data) < $nbFieldsRequired) {
-                return false;
-            }
-        }
+		$nbFieldsRequired = count(Header::FIELDS_REQUIRED);
+		while(($data = fgetcsv($handle, 1000, Separator::COMMA)) !== false) {
+			if (count($data) < $nbFieldsRequired) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
