57a1dc26df5d58e6e63d60ad8bd338bfa8ddf09f
zak39 <fotia.baptiste@hotmail.com>
2025-05-14 17:33:50 +0200

    feat(api): Find a workspace using OCS API

--- lib/Controller/WorkspaceApiOcsController.php
+++ lib/Controller/WorkspaceApiOcsController.php
@@ -24,14 +24,104 @@
 
 namespace OCA\Workspace\Controller;
 
-use OCP\AppFramework\OCSController;
 use OCP\IRequest;
+use OCP\AppFramework\Http;
+use Psr\Log\LoggerInterface;
+use OCA\Workspace\Db\SpaceMapper;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\OCSController;
+use OCA\Workspace\Folder\RootFolder;
+use OCP\AppFramework\Http\DataResponse;
+use OCA\Workspace\Service\Group\UserGroup;
+use OCA\Workspace\Helper\GroupfolderHelper;
+use OCA\Workspace\Service\Group\GroupFormatter;
+use OCA\Workspace\Service\WorkspaceService;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\IGroupManager;
 
 class WorkspaceApiOcsController extends OCSController {
 	public function __construct(
 		IRequest $request,
+		private GroupfolderHelper $folderHelper,
+		private IGroupManager $groupManager,
+		private LoggerInterface $logger,
+		private RootFolder $rootFolder,
+		private SpaceMapper $spaceMapper,
+		private WorkspaceService $workspaceService,
 		public $appName,
 	) {
 		parent::__construct($appName, $request);
+	}
+
+	/**
+	 * @return Response<{
+	 * 	id: int,
+	 * 	mount_point: string,
+	 * 	groups: array,
+	 * 	quota: int,
+	 * 	size: int,
+	 * 	acl: bool,
+	 * 	manage: array,
+	 * 	groupfolder_id: int,
+	 * 	name: string,
+	 * 	color_code: string,
+	 * 	users: array,
+	 * 	userCount: int,
+	 * 	added_groups: array
+	 * }, Http::STATUS_OK>
+	 * 
+	 * 200: Workspace returned
+	 */
+	#[NoAdminRequired]
+	#[FrontpageRoute(
+		verb: 'GET',
+		url: '/api/v1/spaces/{id}',
+		requirements: ['id' => '\d+']
+	)]
+	public function find(int $id): Response {
+		$space = $this->spaceMapper->find($id);
+
+		$groupfolder = $this->folderHelper->getFolder($space->getGroupfolderId(), $this->rootFolder->getRootFolderStorageId());
+
+		if ($groupfolder === false) {
+			return new DataResponse(
+				[
+					'message' => 'Failed loading groupfolder ' . $space->getGroupfolderId(),
+					'success' => false
+				],
+				Http::STATUS_BAD_REQUEST
+			);
+		}
+
+		$space = array_merge($groupfolder, $space->jsonSerialize());
+
+		$gids = array_keys($space['groups']) ?? [];
+		$wsGroups = [];
+		$addedGroups = [];
+		
+		foreach ($gids as $gid) {
+			$group = $this->groupManager->get($gid);
+
+			if (UserGroup::isWorkspaceGroup($group)) {
+				$wsGroups[] = $group;
+			} else {
+				$addedGroups[] = $group;
+			}
+	
+			if (UserGroup::isWorkspaceUserGroupId($gid)) {
+				$space['userCount'] = $group->count();
+			}
+		}
+
+		$users = $this->workspaceService->addUsersInfo($space);
+		$space['users'] = $users;
+
+		$space['groups'] = GroupFormatter::formatGroups($wsGroups);
+		$space['added_groups'] = (object)GroupFormatter::formatGroups($addedGroups);
+
+		$this->logger->info("Find the workspace with {$id} successfully", $space);
+
+		return new DataResponse($space, Http::STATUS_OK);
 	}
 }
