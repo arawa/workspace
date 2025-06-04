1b113bf4b5e04dd3a3f72d8ae19ec9e1d3b3ae85
zak39 <fotia.baptiste@hotmail.com>
2025-06-04 16:59:58 +0200

    feat(api): Implement subgroup creation using OCS API

--- lib/Controller/WorkspaceApiOcsController.php
+++ lib/Controller/WorkspaceApiOcsController.php
@@ -24,6 +24,13 @@
 
 namespace OCA\Workspace\Controller;
 
+use OCA\Workspace\Attribute\WorkspaceManagerRequired;
+use OCA\Workspace\Space\SpaceManager;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\Response;
 use OCP\AppFramework\OCSController;
 use OCP\IRequest;
 
@@ -31,7 +38,23 @@ class WorkspaceApiOcsController extends OCSController {
 	public function __construct(
 		IRequest $request,
 		public $appName,
+		private SpaceManager $spaceManager,
 	) {
 		parent::__construct($appName, $request);
+	}
+
+	#[NoAdminRequired]
+	#[WorkspaceManagerRequired]
+	#[FrontpageRoute(
+		verb: 'POST',
+		url: '/api/v1/space/{id}/groups',
+		requirements: [
+			'id' => '\d+',
+			'groupname' => '[A-Za-z-0-9].*'
+		]
+	)]
+	public function createSubGroup(int $id, string $groupname): Response {
+		$group = $this->spaceManager->createSubGroup($id, $groupname);
+		return new DataResponse([ 'gid' => $group->getGID() ], Http::STATUS_CREATED);
 	}
 }
