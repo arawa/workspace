72ff87f422775bfd0dd6cb02a2cbb271ed1cd344
zak39 <fotia.baptiste@hotmail.com>
2025-06-10 17:39:51 +0200

    refactor: Move the main code from controller to spaceManager class
    I moved the main code as well as some of unit test code.
    
    I fixed a bug that occured when removing a user who is workspace managers for two different spaces.

--- lib/Controller/WorkspaceApiOcsController.php
+++ lib/Controller/WorkspaceApiOcsController.php
@@ -27,24 +27,20 @@ namespace OCA\Workspace\Controller;
 use OCA\Workspace\Attribute\RequireExistingSpace;
 use OCA\Workspace\Attribute\SpaceIdNumber;
 use OCA\Workspace\Attribute\WorkspaceManagerRequired;
-use OCA\Workspace\Service\Group\UserGroup;
+use OCA\Workspace\Space\SpaceManager;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\FrontpageRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Http\Response;
-use OCP\AppFramework\OCS\OCSBadRequestException;
 use OCP\AppFramework\OCSController;
-use OCP\IGroupManager;
 use OCP\IRequest;
-use OCP\IUserManager;
 use Psr\Log\LoggerInterface;
 
 class WorkspaceApiOcsController extends OCSController {
 	public function __construct(
 		IRequest $request,
-		private IGroupManager $groupManager,
-		private IUserManager $userManager,
+		private SpaceManager $spaceManager,
 		private LoggerInterface $logger,
 		public $appName,
 	) {
@@ -61,43 +57,9 @@ class WorkspaceApiOcsController extends OCSController {
 		requirements: ['id' => '\d+']
 	)]
 	public function removeUsersInWorkspace(int $id, array $uids): Response {
-		$types = array_unique(array_map(fn ($uid) => gettype($uid), $uids));
-		$othersStringTypes = array_values(array_filter($types, fn ($type) => $type !== 'string'));
-
-		if (!empty($othersStringTypes)) {
-			throw new OCSBadRequestException('uids params must contain a string array only');
-		}
-
-		$usersNotExist = [];
-		foreach ($uids as $uid) {
-			$user = $this->userManager->get($uid);
-			if (is_null($user)) {
-				$usersNotExist[] = $uid;
-			}
-		}
-
-		if (!empty($usersNotExist)) {
-			$formattedUsers = implode(array_map(fn ($user) => "- {$user}" . PHP_EOL, $usersNotExist));
-			$this->logger->error('These users not exist in your Nextcoud instance : ' . PHP_EOL . $formattedUsers);
-			throw new OCSBadRequestException('These users not exist in your Nextcoud instance : ' . PHP_EOL . $formattedUsers);
-		}
-
-		$gid = UserGroup::get($id);
-
-		$userGroup = $this->groupManager->get($gid);
-
-		if (is_null($userGroup)) {
-			$this->logger->error("The group with {$gid} group doesn't exist.");
-			throw new OCSBadRequestException("The group with {$gid} group doesn't exist.");
-		}
-
-		$users = array_map(fn ($uid) => $this->userManager->get($uid), $uids);
-
-		foreach ($users as $user) {
-			$uid = $user->getUID();
-			$userGroup->removeUser($user);
-			$this->logger->info("The user with the uid {$uid} has been removed from the group {$gid}");
-		}
+		$this->spaceManager->removeUsersFromWorkspace($id, $uids);
+		$uidsStringify = implode(', ', $uids);
+		$this->logger->info("These users are removed from the workspace with the id {$id} : {$uidsStringify}");
 
 		return new DataResponse([], Http::STATUS_NO_CONTENT);
 	}
--- tests/Unit/Controller/WorkspaceApiOcsControllerTest.php
+++ tests/Unit/Controller/WorkspaceApiOcsControllerTest.php
@@ -27,6 +27,7 @@ namespace OCA\Workspace\Tests\Unit\Controller;
 use Mockery;
 use OCA\Workspace\Controller\WorkspaceApiOcsController;
 use OCA\Workspace\Service\Group\UserGroup;
+use OCA\Workspace\Space\SpaceManager;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\IGroup;
@@ -40,24 +41,21 @@ use Psr\Log\LoggerInterface;
 
 class WorkspaceApiOcsControllerTest extends TestCase {
 
-	private IGroupManager&MockObject $groupManager;
 	private IRequest&MockObject $request;
-	private IUserManager&MockObject $userManager;
+	private SpaceManager&MockObject $spaceManager;
 	private LoggerInterface&MockObject $logger;
 	private string $appName;
 	private WorkspaceApiOcsController $controller;
 	
 	public function setUp(): void {
 		$this->appName = 'workspace';
-		$this->groupManager = $this->createMock(IGroupManager::class);
 		$this->logger = $this->createMock(LoggerInterface::class);
 		$this->request = $this->createMock(IRequest::class);
-		$this->userManager = $this->createMock(IUserManager::class);
+		$this->spaceManager = $this->createMock(SpaceManager::class);
 		
 		$this->controller = new WorkspaceApiOcsController(
 			$this->request,
-			$this->groupManager,
-			$this->userManager,
+			$this->spaceManager,
 			$this->logger,
 			$this->appName
 		);
@@ -70,51 +68,6 @@ class WorkspaceApiOcsControllerTest extends TestCase {
 	public function testRemoveUsersToWorkspaceReturnsValidResponse(): void {
 		$spaceId = 1;
 		$uids = ['user1', 'user2'];
-
-		/** @var IUser&MockObject */
-		$user1 = $this->createMock(IUser::class);
-		/** @var IUser&MockObject */
-		$user2 = $this->createMock(IUser::class);
-				
-		$this->userManager
-			->expects($this->any())
-			->method('get')
-			->with(
-				$this->logicalOr($this->equalTo('user1'), $this->equalTo('user2'))
-			)
-			->willReturnOnConsecutiveCalls($user1, $user2, $user1, $user2)
-		;
-		
-		$groupUser = $this->createMock(IGroup::class);
-		$groupWorkspaceManagerUser = $this->createMock(IGroup::class);
-
-		$this->groupManager
-			->expects($this->any())
-			->method('get')
-			->willReturn($groupUser, $groupWorkspaceManagerUser)
-		;
-
-		$groupFormatter = Mockery::mock(UserGroup::class);
-		$groupFormatter
-			->shouldReceive('get')
-			->with($spaceId)
-			->andReturn('SPACE-U-1')
-		;
-		
-		/** @var IGroup&MockObject */
-		$userGroup = $this->createMock(IGroup::class);
-
-		$this->groupManager
-			->expects($this->once())
-			->method('get')
-			->willReturn($userGroup)
-		;
-
-		$groupUser
-			->expects($this->any())
-			->method('addUser')
-			->with($this->logicalOr($this->equalTo($user1), $this->equalTo($user2)))
-		;
 
 		$actual = $this->controller->removeUsersInWorkspace($spaceId, $uids);
 
