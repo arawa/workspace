ddc336974f3f1159c824003e40d9356be2cf4300
zak39 <fotia.baptiste@hotmail.com>
2025-06-19 17:08:10 +0200

    feat: Implement function to remove users from groups in space with OCS API

--- lib/Controller/WorkspaceApiOcsController.php
+++ lib/Controller/WorkspaceApiOcsController.php
@@ -24,14 +24,81 @@
 
 namespace OCA\Workspace\Controller;
 
+use OCA\Workspace\Attribute\RequireExistingSpace;
+use OCA\Workspace\Attribute\SpaceIdNumber;
+use OCA\Workspace\Attribute\WorkspaceManagerRequired;
+use OCA\Workspace\Service\Group\GroupsWorkspace;
+use OCA\Workspace\Space\SpaceManager;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\OCS\OCSBadRequestException;
+use OCP\AppFramework\OCS\OCSNotFoundException;
 use OCP\AppFramework\OCSController;
+use OCP\IGroupManager;
 use OCP\IRequest;
+use OCP\IUserManager;
 
 class WorkspaceApiOcsController extends OCSController {
 	public function __construct(
 		IRequest $request,
+		private IGroupManager $groupManager,
+		private IUserManager $userManager,
+		private SpaceManager $spaceManager,
 		public $appName,
 	) {
 		parent::__construct($appName, $request);
+	}
+
+	#[SpaceIdNumber]
+	#[RequireExistingSpace]
+	#[WorkspaceManagerRequired]
+	#[FrontpageRoute(
+		verb: 'DELETE',
+		url: '/api/v1/space/{id}/group/{gid}',
+		requirements: [
+			'id' => '\d+',
+			'gid' => '^SPACE-[A-Za-z0-9\W]+'
+		]
+	)]
+	public function removeUsersFromGroup(int $id, string $gid, array $uids): Response {
+		$users = [];
+		$group = $this->groupManager->get($gid);
+
+		$usersNotFound = [];
+		foreach ($uids as $uid) {
+			$user = $this->userManager->get($uid);
+			if (is_null($user)) {
+				$usersNotFound[] = $uid;
+			}
+
+			$users[] = $user;
+		}
+
+		if (!empty($usersNotFound)) {
+			$usersNotFound = array_map(fn ($uid) => "- {$uid}", $usersNotFound);
+			$usersNotFound = implode("\n", $usersNotFound);
+			throw new OCSNotFoundException("These users not exist in your Nextcloud instance:\n{$usersNotFound}");
+		}
+
+		$users = array_map(fn ($uid) => $this->userManager->get($uid), $uids);
+
+		switch ($gid) {
+			case GroupsWorkspace::isWorkspaceUserGroupId($gid):
+				$space = $this->spaceManager->get($id);
+				$this->spaceManager->removeUsersFromUserGroup($space, $group, $users);
+				break;
+			case GroupsWorkspace::isWorkspaceSubGroup($gid) || GroupsWorkspace::isWorkspaceGroup($group):
+				$this->spaceManager->removeUsersFromSubGroup($group, $users);
+				break;
+			case GroupsWorkspace::isWorkspaceAdminGroupId($gid):
+				$this->spaceManager->removeUsersFromWorkspaceManagerGroup($group, $users);
+				break;
+			default:
+				throw new OCSBadRequestException("Your gid {$gid} doesn't come from a workspace");
+		}
+
+		return new DataResponse([], Http::STATUS_OK);
 	}
 }
