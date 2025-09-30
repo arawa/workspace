2e14e2b9f1f2ffcbeee13d205ffd3ac1853859bf
zak39 <fotia.baptiste@hotmail.com>
2025-09-30 12:11:10 +0200

    chore: composer run cs:fix

--- tests/Unit/Controller/WorkspaceApiOcsControllerTest.php
+++ tests/Unit/Controller/WorkspaceApiOcsControllerTest.php
@@ -861,7 +861,7 @@ class WorkspaceApiOcsControllerTest extends TestCase {
 				'added_groups' => (object)[],
 			])
 		;
-		
+
 		$this->spaceManager
 			->expects($this->once())
 			->method('remove')
@@ -2565,7 +2565,7 @@ class WorkspaceApiOcsControllerTest extends TestCase {
 			->method('removeGroup')
 			->with($group)
 		;
-	
+
 		$actual = $this->controller->removeGroup($id, $gid);
 
 		$expected = new DataResponse([], Http::STATUS_NO_CONTENT);
