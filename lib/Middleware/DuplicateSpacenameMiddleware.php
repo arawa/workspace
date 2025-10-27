a12c0968164b132bc5ca2747e20b898dd22c7477
zak39 <fotia.baptiste@hotmail.com>
2025-10-27 15:12:05 +0100

    refactor(middleware): Use a single if conditionnal for DuplicateSpacenameMiddleware
    link: https://github.com/arawa/workspace/pull/1414#discussion_r2459314116

--- lib/Middleware/DuplicateSpacenameMiddleware.php
+++ lib/Middleware/DuplicateSpacenameMiddleware.php
@@ -13,19 +13,20 @@ use OCP\AppFramework\OCSController;
 class DuplicateSpacenameMiddleware extends Middleware {
 
 	public function afterException(Controller $controller, string $methodName, Exception $exception): Response {
-		if ($controller instanceof OCSController && $exception instanceof SpacenameExistException) {
-			return new JSONResponse([
-				'message' => "This space or groupfolder already exists. Please, use another space name.\nIf a \"toto\" space exists, you cannot create the \"tOTo\" space.\nPlease check also the groupfolder doesn't exist."
-			], $exception->getCode());
-		}
-
 		if ($exception instanceof SpacenameExistException) {
-			return new JSONResponse([
-				'title' => 'Error - Duplicate space name',
-				'statuscode' => $exception->getCode(),
-				'message' => "This space or groupfolder already exists. Please, use another space name.\nIf a \"toto\" space exists, you cannot create the \"tOTo\" space.\nPlease check also the groupfolder doesn't exist.",
-				'args_message' => []
-			], $exception->getCode());
+			$message = "This space or groupfolder already exists. Please, use another space name.\nIf a \"toto\" space exists, you cannot create the \"tOTo\" space.\nPlease check also the groupfolder doesn't exist.";
+
+			$data = $controller instanceof OCSController
+				? [ 'message' => $message ]
+				: [
+					'title' => 'Error - Duplicate space name',
+					'statuscode' => $exception->getCode(),
+					'message' => $message,
+					'args_message' => []
+				]
+			;
+
+			return new JSONResponse($data, $exception->getCode());
 		}
 
 		throw $exception;
