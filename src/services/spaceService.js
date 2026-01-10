4b3a5b3a0517652a6b9419e3bcda6f56f25870ed
rakekniven <2069590+rakekniven@users.noreply.github.com>
2026-01-10 16:12:27 +0100

    chore(i18n): remove space before questionmark
    Reported at Transifex

--- src/services/spaceService.js
+++ src/services/spaceService.js
@@ -148,9 +148,9 @@ export function addGroupToWorkspace(spaceId, gid) {
 		.catch(error => {
 			showNotificationError(
 				t('workspace', 'Error groups'),
-				t('workspace', 'Impossible to attach the {gid} group to workspace. May be a problem with the connection ?', { gid }),
+				t('workspace', 'Impossible to attach the {gid} group to workspace. May be a problem with the connection?', { gid }),
 				5000)
-			console.error('Impossible to attach the group to workspace. May be a problem with the connection ?', gid, error)
+			console.error('Impossible to attach the group to workspace. May be a problem with the connection?', gid, error)
 			throw new AddGroupToGroupfolderError('Error to add Space Manager group in the groupfolder')
 		})
 }
@@ -166,7 +166,7 @@ export function removeWorkspace(spaceId) {
 			return resp.data
 		})
 		.catch(error => {
-			console.error('Error to delete a workspace. May be a problem network ?', error)
+			console.error('Error to delete a workspace. May be a problem network?', error)
 		})
 	return result
 }
