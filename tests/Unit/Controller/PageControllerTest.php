ad3aa7201678c72b9954fdce68f3a229fb60175b
zak39 <fotia.baptiste@hotmail.com>
2026-03-05 11:19:37 +0100

    test(php): Update unit test for PageController#index

--- tests/Unit/Controller/PageControllerTest.php
+++ tests/Unit/Controller/PageControllerTest.php
@@ -27,8 +27,13 @@ namespace OCA\Workspace\Tests\Unit\Controller;
 
 use OCA\Workspace\Controller\PageController;
 use OCA\Workspace\Service\UserService;
+use OCA\Workspace\Space\SpaceManager;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\IConfig;
+use OCP\IGroup;
+use OCP\IGroupManager;
+use OCP\IUser;
 use OCP\IUserSession;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -38,21 +43,67 @@ class PageControllerTest extends TestCase {
 	private $userId = 'john';
 	private MockObject&UserService $userService;
 	private MockObject&IConfig $config;
+	private MockObject&IInitialState $initialState;
+	private MockObject&SpaceManager $spaceManager;
+	private MockObject&IGroupManager $groupManager;
 	private MockObject&IUserSession $session;
 
 	public function setUp(): void {
 		$this->userService = $this->createMock(UserService::class);
 		$this->config = $this->createMock(IConfig::class);
 		$this->session = $this->createMock(IUserSession::class);
+		$this->initialState = $this->createMock(IInitialState::class);
+		$this->spaceManager = $this->createMock(SpaceManager::class);
+		$this->groupManager = $this->createMock(IGroupManager::class);
 
 		$this->controller = new PageController(
 			$this->userService,
 			$this->config,
+			$this->initialState,
 			$this->session,
+			$this->spaceManager,
+			$this->groupManager
 		);
 	}
 
 	public function testIndex(): void {
+		$gid = 'GeneralManager';
+		$user = $this->createMock(IUser::class);
+
+		$this->session
+			->expects($this->exactly(2))
+			->method('getUser')
+			->willReturn($user)
+		;
+
+		$user
+			->expects($this->any())
+			->method('getUID')
+			->willReturn($this->userId)
+		;
+
+		$generalManagerGroupMocked = $this->createMock(IGroup::class);
+
+		$this->groupManager
+			->expects($this->once())
+			->method('get')
+			->with($gid)
+			->willReturn($generalManagerGroupMocked)
+		;
+
+		$generalManagerGroupMocked
+			->expects($this->once())
+			->method('inGroup')
+			->with($user)
+			->willReturn(true)
+		;
+
+		$this->spaceManager
+			->expects($this->once())
+			->method('countWorkspaces')
+			->willReturn(5)
+		;
+
 		$result = $this->controller->index();
 
 		$this->assertEquals('index', $result->getTemplateName());
